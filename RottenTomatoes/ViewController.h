ab5e337a2b32e261c8d5ef23196362833ef3ba44
Vikas Kumar Bajaj <vikaskbajaj@gmail.com>
2014-09-15 11:06:22 -0700

    Initial Commit

---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Vikas Kumar Bajaj on 9/15/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RottenTomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RottenTomatoes
+//
+//  Created by Vikas Kumar Bajaj on 9/15/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
